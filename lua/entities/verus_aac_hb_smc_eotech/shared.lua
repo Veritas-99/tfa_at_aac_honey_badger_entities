63b3ccef933e663c5549ec93bf3a6379d0025ddb
Nicolas Wasserkampf <nicolaswasserkampf@gmail.com>
2021-02-21 10:44:05 +0100

    v1

---
+++ lua/entities/verus_aac_hb_smc_eotech/shared.lua
@@ -0,0 +1,15 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "EOTech Sights"
+ENT.Spawnable = true
+ENT.RenderGroup = RENDERGROUP_BOTH
+ENT.Category = "[TFA][AT] AAC Honey Badger"
+ENT.Attachment = "smc_eotech"
+
+function ENT:compatible(wep)
+    if wep.Attach and wep.AttachmentCache[self.Attachment] ~= nil then
+        return true
+    else
+        return false
+    end
+end
